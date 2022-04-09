8da2c763bf29e4e6da91f57e525f935520a84412
Sanjay R <sanjayprashanth1997@gmail.com>
2022-04-09 23:06:09 +0530

    .env PORT settings

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ import { DashRouter } from "./routes/dashBoad.js";
 dotenv.config();
 
 const app = express();
-const PORT = 8100;
+const PORT = process.env.PORT;
 
 //Mongo connection
 MongoConnect();
